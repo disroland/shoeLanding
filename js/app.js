fa7683af73519409c51402b1a1b8152406b87772
admin <admin@admins-MacBook-Pro.local>
2020-01-23 14:28:07 +0200

    gallery

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@
 // 	mnu.classList.toggle('active');
 // }
 
+new WOW().init();   
 
 //slider
 $(document).ready(function(){
@@ -45,7 +46,7 @@ $(document).ready(function(){
 		nxt();
 		prev();
 
-	document.querySelector('div').addEventListener('mouseup', function(){
+	document.querySelector('.cont').addEventListener('mouseup', function(){
 		$('.left').on('click', function(){
 			if (i>0) {i=i-1;
 			sld();} else {i=im.length-1; sld();}
@@ -67,3 +68,58 @@ function showMnu(){
 	mnu.classList.toggle('active');
 }
 })
+
+//modal
+$('.card img').on('click', function(e)
+{
+  let $imgSrc = $(this).attr('src');
+  let $modalImage = $('<img>');
+  $modalImage.attr('src', $imgSrc).addClass('ba-modal__image');
+  $('.ba-modal-content-wrapper').append($modalImage);
+  openModal();
+}
+)
+
+let openModal = function(){
+  $('.ba-modal').addClass('ba-modal-open');
+  $('body').addClass('ba-modal-is-open');
+  $(document).on('keydown', function (e){
+    if (e.keyCode == 27){
+      closeModal();
+    }
+  })
+}
+let closeModal = function(){
+  $('.ba-modal').removeClass('ba-modal-open');
+  $('body').removeClass('ba-modal-is-open');
+  $('.ba-modal-content-wrapper').empty();
+}
+$('.ba-modal-close').on('click', closeModal);
+$('.ba-modal').on('click', function(e){
+  let modalContent = $('.ba-modal-content');
+  if (!modalContent.is(e.target) && modalContent.has(e.target).length==0){
+    closeModal();
+  }
+})
+
+
+//header img
+// let headPicture = document.querySelector('.header-picture img');
+// let headImg = document.querySelector('.wrap .center');
+// let HeadAttr;
+
+// $(window).on('mousemove', function(){
+// 	HeadAttr = headImg.getAttribute('src');
+// 	headPicture.setAttribute('src', HeadAttr);
+// })
+
+//header-animation
+
+$('.header-picture img').on('click', function(){
+	$(this).addClass('animate');
+})
+
+$('.header-picture img').on('mousemove', function(){
+	$(this).removeClass('animate');
+})
+
